ff7692a496cba69afe50badc70e6a958decbdf60
Steve.K.Y <122028234+Steven-Yabann@users.noreply.github.com>
2025-01-21 17:53:47 +0300

    Worked on the naive bayes model

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,7 +12,7 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("./tools/")
 from email_preprocess import preprocess
 
 
@@ -24,8 +24,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+# Create the classifier
+clf = GaussianNB()
 
+# Fit the classifier
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+# Predict using the test 
+t0 = time()
+prediction = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(prediction, labels_test)
+
+print(f'accuracy: {accuracy}')
 
 ##############################################################
 
@@ -36,12 +54,12 @@ The Code Given on Udacity Website is in Python-2
 The Following Code is Python-3 version of the same code
 '''
 
-# t0 = time()
-# # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
-
-# t0 = time()
-# # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
-
+'''
+.venvstevenyabann@Stevens-MacBook-Pro ud120-projects % python3 naive_bayes/nb_author_id.py
+No. of Chris training emails :  7936
+No. of Sara training emails :  7884
+Training Time: 0.325 s
+Predicting Time: 0.031 s
+accuracy: $0.9732650739476678
+'''
 ##############################################################
