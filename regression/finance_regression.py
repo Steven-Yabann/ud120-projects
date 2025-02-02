b29aa48f7e10afc708360985c5d2789ad8ea8b2e
Steve.K.Y <122028234+Steven-Yabann@users.noreply.github.com>
2025-02-02 17:59:25 +0300

    Worked on regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -15,34 +15,35 @@
 import os
 import sys
 import joblib
-sys.path.append(os.path.abspath("../tools/"))
+sys.path.append(os.path.abspath("./tools/"))
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = joblib.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
+
+dictionary = joblib.load( open("./final_project/final_project_dataset_modified.pkl", "rb") )
 
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = './tools/python2_lesson06_keys.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
-
+test_color = "r"
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn import linear_model
+from sklearn.metrics import r2_score
 
+reg = linear_model.LinearRegression()
 
-
-
+reg.fit(feature_train, target_train)
 
 
 
@@ -65,6 +66,11 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+print(f'The coefficient is: {reg.coef_}')
+print(f'The intercept is: {reg.intercept_}')
+print(f'The r-2 score with training data is {reg.score(feature_test, target_test)}')
+plt.plot(feature_train, reg.predict(feature_train), color="y")
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
